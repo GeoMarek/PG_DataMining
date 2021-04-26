b34e4b2cdf851ffd2c13ad3073d01d14fef34704
Marek Grudkowski <mar.grudkowski@gmail.com>
2021-04-26 19:41:54 +0200

    clean raport branch

--- init_venv.bat
+++
@@ -1,6 +0,0 @@
-if not exist virtual_environment\ (
-	python -m venv virtual_environment
-	virtual_environment\Scripts\activate.bat & pip install numpy pandas matplotlib seaborn jupyter tqdm plotly statsmodels
-)
-virtual_environment\Scripts\jupyter notebook
-
