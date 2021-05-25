456798451bf458667172252803c2b8762749c84e
Marek Grudkowski <mar.grudkowski@gmail.com>
2021-05-25 23:17:45 +0200

    add typing and docstrings

--- Countries.py
+++ Countries.py
@@ -1,10 +1,11 @@
 import math
 import pandas as pd
+from pandas import DataFrame
 
 
 class Country:
 
-    def __init__(self, data_frame, country):
+    def __init__(self, data_frame: DataFrame, country: str):
         self.df = data_frame.loc[data_frame['country'] == country]
         self.name = country
         self.population = 0
@@ -21,48 +22,20 @@ class Country:
         self.fillDailyVaccinations()
         self.fillDailyVaccinationsPerMillion()
 
-        # fill people vaccinated, not implemented
+        # fill people vaccinated
         self.fillPeopleVaccinated()
         self.fillPeopleVaccinatedPerHundred()
-
-        # fill people fully vaccinated, not implemented
-        self.fillPeopleFullyVaccinated()
         self.fillPeopleFullyVaccinatedPerHundred()
 
-        # check for missings
-        self.showMisses()
-
-    def showMisses(self):
-        x1 = self.df['total_vaccinations'].isna().sum()
-        x2 = self.df['total_vaccinations_per_hundred'].isna().sum()
-        x3 = self.df['daily_vaccinations'].isna().sum()
-        x4 = self.df['daily_vaccinations_per_million'].isna().sum()
-        x5 = self.df['people_vaccinated'].isna().sum()
-        x6 = self.df['people_vaccinated_per_hundred'].isna().sum()
-        x7 = self.df['people_fully_vaccinated'].isna().sum()
-        x8 = self.df['people_fully_vaccinated_per_hundred'].isna().sum()
-
-        if any([x1, x2, x3, x4, x5, x6, x7, x8]) > 0:
-            print(f"Country {self.name.upper()} has missings")
-            self._showMissingIndices(x1, 'total_vaccinations')
-            self._showMissingIndices(x2, 'total_vaccinations_per_hundred')
-            self._showMissingIndices(x3, 'daily_vaccinations')
-            self._showMissingIndices(x4, 'daily_vaccinations_per_million')
-            self._showMissingIndices(x5, 'people_vaccinated')
-            self._showMissingIndices(x6, 'people_vaccinated_per_hundred')
-            self._showMissingIndices(x7, 'people_fully_vaccinated')
-            self._showMissingIndices(x8, 'people_fully_vaccinated_per_hundred')
-
-    def fillTotalVaccinations(self):
-        dfc = self.df.copy()
-        dfc['total_vaccinations'] = dfc['total_vaccinations'].interpolate()
-        self.df = dfc.copy()
+    def fillTotalVaccinations(self) -> None:
+        self._interpolateBy('total_vaccinations')
         self._updateBeginningOf('total_vaccinations')
+        self.df['total_vaccinations'] = self.df['total_vaccinations'].astype(int)
 
-    def fillTotalVaccinationsPerHundred(self):
+    def fillTotalVaccinationsPerHundred(self) -> None:
         self._fillPairedColumns('total_vaccinations', 'total_vaccinations_per_hundred')
 
-    def fillDailyVaccinations(self):
+    def fillDailyVaccinations(self) -> None:
         start_id = None
         column = 'daily_vaccinations'
         other_column = 'total_vaccinations'
@@ -78,36 +51,82 @@ class Country:
                     value = self._movingAverageForDailyVaccinations(x2 - x1, index, start_id)
                 self.df.at[index, column] = value
         self._updateBeginningOf('daily_vaccinations')
+        self.df['daily_vaccinations'] = self.df['daily_vaccinations'].astype(int)
 
-    def fillDailyVaccinationsPerMillion(self):
+    def fillDailyVaccinationsPerMillion(self) -> None:
         self._fillPairedColumns('daily_vaccinations', 'daily_vaccinations_per_million', coef=1_000_000)
 
-    def fillPeopleVaccinated(self):
-        # TODO: implement
-        pass
+    def fillPeopleVaccinated(self) -> None:
+        self._fillRowsWithPeopleVaccinated()
+        self._interpolateBy('people_vaccinated')
+        self._updateBeginningOf('people_vaccinated')
+        self._fillRowsWithPeopleVaccinated()
 
-    def fillPeopleVaccinatedPerHundred(self):
+    def fillPeopleVaccinatedPerHundred(self) -> None:
         self._fillPairedColumns('people_vaccinated', 'people_vaccinated_per_hundred')
 
-    def fillPeopleFullyVaccinated(self):
-        # TODO: implement
-        pass
-
-    def fillPeopleFullyVaccinatedPerHundred(self):
+    def fillPeopleFullyVaccinatedPerHundred(self) -> None:
         self._fillPairedColumns('people_fully_vaccinated', 'people_fully_vaccinated_per_hundred')
 
-    def _updateBeginningOf(self, column):
+    def _fillRowsWithPeopleVaccinated(self) -> None:
+        """
+        Fill in the columns that are dependent on each other:
+        `peopleWithTwo = allPeople - peopleWithOne`
+        `peopleWithOne = allPeople - peopleWithTwo`
+        """
+        for index, row in self.df.iterrows():
+            total = self.df.at[index, 'total_vaccinations']
+            people = self.df.at[index, 'people_vaccinated']
+            fully = self.df.at[index, 'people_fully_vaccinated']
+            is_people_nan = math.isnan(people)
+            is_fully_nan = math.isnan(fully)
+            if people > total:
+                self.df.at[index, 'total_vaccinations'] = people
+                if is_fully_nan:
+                    self.df.at[index, 'people_fully_vaccinated'] = 0
+            elif (not is_people_nan) and is_fully_nan:
+                fully = total - people
+                self.df.at[index, 'people_fully_vaccinated'] = fully
+            elif is_people_nan and (not is_fully_nan):
+                people = total - fully
+                self.df.at[index, 'people_vaccinated'] = people
+
+    def _interpolateBy(self, column_name: str) -> None:
+        """
+        Update data frame in column using default interpolation method
+
+        :param column_name: name of column
+        """
+        dfc = self.df.copy()
+        dfc[column_name] = dfc[column_name].interpolate()
+        self.df = dfc.copy()
+
+    def _updateBeginningOf(self, column_name: str) -> None:
+        """
+        Update data frame in specific column if at the beginning is NaN value.
+        Fill this NaN value with 0 and then interpolate values in column.
+
+        :param column_name: name of column
+
+        """
         first_null_index = None
         for index, row in self.df.iterrows():
             if first_null_index is None:
                 first_null_index = index
                 break
-        self.df.at[first_null_index, column] = 0
-        dfc = self.df.copy()
-        dfc[column] = dfc[column].interpolate()
-        self.df = dfc.copy()
+        self.df.at[first_null_index, column_name] = 0
 
-    def _movingAverageForDailyVaccinations(self, value, index, start_index):
+        self._interpolateBy(column_name)
+
+    def _movingAverageForDailyVaccinations(self, value: int, index: int, start_index: int) -> int:
+        """
+        Calculate moving average using 6 values from behind and one from diff
+
+        :param value: difference between total vaccinations
+        :param index: numer of row in which will be new value
+        :param start_index: index at first row
+        :return: moving average
+        """
         count = 1
         vacc_sum = value
         while index - 1 < start_index:
@@ -116,15 +135,25 @@ class Country:
             count += 1
             if count == 7:
                 break
-        return vacc_sum / count
+        return vacc_sum // count
 
-    def _dropUnnecesaryColumns(self):
+    def _dropUnnecesaryColumns(self) -> None:
+        """
+        Drop useless columns
+        """
         del self.df['source_name']
         del self.df['source_website']
         del self.df['vaccines']
         del self.df['daily_vaccinations_raw']
 
-    def _getPopulationBasedOn(self, column):
+    def _getPopulationBasedOn(self, column: str) -> int:
+        """
+        Returns population based on equation:
+         `population = (total / per_hundred) * 100`
+
+        :param column: column on which calculations are based
+        :return: population
+        """
         abs_col = column
         rel_col = column + "_per_hundred"
         absolute = None
@@ -139,7 +168,10 @@ class Country:
         else:
             return int(sum(div_sum) / len(div_sum)) * 100
 
-    def _setPrettyAccuratePopulation(self):
+    def _setPrettyAccuratePopulation(self) -> None:
+        """
+        Set population based on three columns. If some of them is NaN, don't include it
+        """
         avg = [self._getPopulationBasedOn('total_vaccinations'),
                self._getPopulationBasedOn('people_vaccinated'),
                self._getPopulationBasedOn('people_fully_vaccinated')
@@ -152,15 +184,18 @@ class Country:
                 avg_size += 1
         self.population = avg_sum // avg_size
 
-    def _fillPairedColumns(self, total_col, rel_col, coef=100):
+    def _fillPairedColumns(self, total_col: str, rel_col: str, coef: int = 100) -> None:
+        """
+        Update data frame relatice column basing on population:
+
+        `x_per_hundred = (x / population) * 100
+
+        :param total_col: column with total values
+        :param rel_col: column with relative values
+        :param coef: per hundred or per ...
+        """
         for index, row in self.df.iterrows():
             if pd.isna(self.df.at[index, rel_col]):
                 actual_total = self.df.at[index, total_col]
                 relative_value = (actual_total / self.population) * coef
                 self.df.at[index, rel_col] = relative_value
-
-    @staticmethod
-    def _showMissingIndices(x, name):
-        if x > 0:
-            print(f"\t{name} has {x} nan values")
-            # print(np.where(self.df[name].isna())[0].tolist())
--- prepareData.py
+++ prepareData.py
@@ -14,5 +14,25 @@ def readData():
 
 if __name__ == "__main__":
     global_df = readData()
+
+    print(global_df['total_vaccinations'].isna().sum())
+    print(global_df['total_vaccinations_per_hundred'].isna().sum())
+    print(global_df['daily_vaccinations'].isna().sum())
+    print(global_df['daily_vaccinations_per_million'].isna().sum())
+    print(global_df['people_vaccinated'].isna().sum())
+    print(global_df['people_vaccinated_per_hundred'].isna().sum())
+    print(global_df['people_fully_vaccinated'].isna().sum())
+    print(global_df['people_fully_vaccinated_per_hundred'].isna().sum())
+
     for country in global_df.country.unique():
         x = Country(global_df, country)
+        global_df.loc[global_df['country'] == country] = x.df
+
+    print(global_df['total_vaccinations'].isna().sum())
+    print(global_df['total_vaccinations_per_hundred'].isna().sum())
+    print(global_df['daily_vaccinations'].isna().sum())
+    print(global_df['daily_vaccinations_per_million'].isna().sum())
+    print(global_df['people_vaccinated'].isna().sum())
+    print(global_df['people_vaccinated_per_hundred'].isna().sum())
+    print(global_df['people_fully_vaccinated'].isna().sum())
+    print(global_df['people_fully_vaccinated_per_hundred'].isna().sum())
